1d1e634c3138976f30c07417eecaa8079afac365
Ankit Biswas <122113753+ANKIT-BISWAS-20@users.noreply.github.com>
2023-05-12 20:19:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,80 @@
+
+
+function gotox(num){
+    var num1=num;
+    switch(num){
+        case 0:
+            window.open("./daalm.pdf","_blank");
+            break;   
+        case 1:
+            window.open("./target1.html","_blank");
+            break;
+        case 2:
+            window.open("./target2.html","_blank");
+            break;
+        case 3:
+            window.open("./target3.html","_blank");
+            break;
+        case 4:
+            window.open("./target4.html","_blank");
+            break;
+        case 5:
+            window.open("./target5.html","_blank");
+            break;
+        case 6:
+            window.open("./target6.html","_blank");
+            break;
+        case 7:
+            window.open("./target7.html","_blank");
+            break;
+        case 8:
+            window.open("./target8.html","_blank");
+            break;
+        case 9:
+            window.open("./target9.html","_blank");
+            break;
+        case 10:
+            window.open("./target10.html","_blank");
+            break;
+        case 11:
+            window.open("./target11.html","_blank");
+            break;
+        case 12:
+            window.open("./target12.html","_blank");
+            break;
+
+    }
+    
+}
+
+function contact(){
+    window.open("https://ankit-biswas-20.github.io/BISWAS-INDUSTRIES","_blank");
+}
+
+function gt(num){
+    switch(num){
+        case 1:
+            window.open("https://www.facebook.com/profile.php?id=100079956308306&mibextid=ZbWKwL","_blank");
+            break;
+        case 2:
+            window.open("https://instagram.com/ankit_b15w45?igshid=ZGUzMzM3NWJiOQ==","_blank");
+            break;
+        case 3:
+            window.open("https://www.youtube.com/@factx9641","_blank");
+            break;
+        case 4:
+            window.open("https://www.linkedin.com/in/ankit-biswas-a03899257","_blank");
+            break;
+        case 5:
+            window.open("https://github.com/ANKIT-BISWAS-20","_blank");
+            break;
+        case 6:
+            window.open("https://ankit-biswas-20.github.io/BISWAS-INDUSTRIES","_blank");
+            break;
+
+    }
+}
+
+
+
+
